d08b4ff201c03c7a3abd4fdc63453d02b5c7def1
DylanZin <422024558@qq.com>
2022-03-05 20:35:07 +0800

    Update config.h

--- keyboard/Omega45/c/config.h
+++ keyboard/Omega45/c/config.h
@@ -26,8 +26,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define CONF_VENDOR_ID 0x4366 /* 配置项目内显示的VendorID */
 #define CONF_PRODUCT_ID 0x0312 /* 配置项目内显示的ProductID */
 #define DEVICE_VER 0x0002 /* 硬件版本 */
-#define MANUFACTURER "Lotlab" /* 硬件制造商，用于蓝牙显示 */
-#define PRODUCT "Omega45C" /* 硬件名词，用于蓝牙显示 */
+#define MANUFACTURER "ZhaozxKB" /* 硬件制造商，用于蓝牙显示 */
+#define PRODUCT "Omega45" /* 硬件名词，用于蓝牙显示 */
 #define MACADDR_SEPRATOR ' ' /* 蓝牙名称后地址的分隔符。若不设置则不显示蓝牙名称后面的地址 */
 
 /* USB HID report parameter */
@@ -44,7 +44,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define KEYBOARD_SCAN_INTERVAL 1 // 键盘最小时间单位TICK (ms)
 #define KEYBOARD_FAST_SCAN_INTERVAL 10 // 通常模式下，多久扫描一次键盘 (ms)
 #define KEYBOARD_SLOW_SCAN_INTERVAL 100 // 慢速模式下，多久扫描一次键盘 (ms)
-#define LED_AUTOOFF_TIME 5 /* LED自动熄灭时长(s)，设为0则不自动熄灭 */
+#define LED_AUTOOFF_TIME 30 /* LED自动熄灭时长(s)，设为0则不自动熄灭 */
 #define DCDC_ENABLED //启用DCDC电源模式
 
 // 键盘额外功能
@@ -69,22 +69,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_FUNCTION
 
 // LED 配置
-#define LED_CAPS 4  //caps led ：5 ； led1、2、3、4：19、20、27、26
-
-// ws2812 RGB 配置
-#define RGB_DI_PIN 10
-#define RGBLED_NUM 8
-#define RGBLIGHT_ANIMATIONS
-#define RGB_PWR_PIN 17 // P-mos
-//#define RGB_PWR_PIN_REVERSE 12 // N-mos
+#define LED_CAPS 4  //caps led ：4 ； led1、2、3：19、20、10
 
 // 3灯指示配置引脚
 #define LED_STATUS_BLE 19
-#define LED_STATUS_CHARGING 18
+#define LED_STATUS_CHARGING 10
 #define LED_STATUS_USB 20
 #define LED_BLE_CHANNEL1 19
 #define LED_BLE_CHANNEL2 20
-#define LED_BLE_CHANNEL3 18
+#define LED_BLE_CHANNEL3 10
 #define LED_POSITIVE // LED上拉驱动
 // 独立硬件按钮
 #define POWER_BUTTON 3
